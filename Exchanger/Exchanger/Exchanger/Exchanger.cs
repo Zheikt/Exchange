5c674a025c1f2f8742e2d66f69ff89afe1a1bec4
Samuel Howsden <samuel.howsden@student.centralia.edu>
2021-04-16 23:11:50 -0700

    Created 12 methods to convert from every currency to each other

--- Exchanger/Exchanger/Exchanger/Exchanger.cs
+++ Exchanger/Exchanger/Exchanger/Exchanger.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exchanger
+﻿namespace Exchanger
 {
     static class Exchanger
     {
+        public static decimal USDtoCAN(decimal USDAmount)
+        {
+            return 1.25427M * USDAmount;
+        }
+        public static decimal USDtoGBP(decimal USDAmount)
+        {
+            return 0.72523M * USDAmount;
+        }
+        public static decimal USDtoEUR(decimal USDAmount)
+        {
+            return 0.83572M * USDAmount;
+        }
+        public static decimal CANtoUSD(decimal CANAmount)
+        {
+            return 0.79728M * CANAmount;
+        }
+        public static decimal CANtoGBP(decimal CANAmount)
+        {
+            return 0.57848M * CANAmount;
+        }
+        public static decimal CANtoEUR(decimal CANAmount)
+        {
+            return 0.66645M * CANAmount;
+        }
+        public static decimal EURtoUSD(decimal EURAmount)
+        {
+            return 1.19648M * EURAmount;
+        }
+        public static decimal EURtoCAN(decimal EURAmount)
+        {
+            return 1.50040M * EURAmount;
+        }
+        public static decimal EURtoGBP(decimal EURAmount)
+        {
+            return 0.86826M * EURAmount;
+        }
+        public static decimal GBPtoUSD(decimal GBPAmount)
+        {
+            return 1.37887M * GBPAmount;
+        }
+        public static decimal GBPtoCAN(decimal GBPAmount)
+        {
+            return 1.72947M * GBPAmount;
+        }
+        public static decimal GBPtoEUR(decimal GBPAmount)
+        {
+            return 1.15195M * GBPAmount;
+        }
+
     }
 }
