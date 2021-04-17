7248e8713935c80af28160760c41e197d287c983
Samuel Howsden <samuel.howsden@student.centralia.edu>
2021-04-16 22:55:22 -0700

    Base Class Set-Up and program creation

---
+++ Exchanger/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Howsden_Exchanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
