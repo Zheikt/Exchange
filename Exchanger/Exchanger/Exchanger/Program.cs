75f90b47878d83393ed598430f01dc32551fe530
Samuel Howsden <samuel.howsden@student.centralia.edu>
2021-04-19 11:13:22 -0700

    Base Creation of the UI on the console. Not completed yet, still need to work out some issues.

--- Exchanger/Exchanger/Exchanger/Program.cs
+++ Exchanger/Exchanger/Exchanger/Program.cs
@@ -6,7 +6,34 @@ namespace Exchanger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ExchangeMonitor monitor = new ExchangeMonitor();
+            do
+            {
+                int exchangeChoice;
+                decimal exchangeInput;
+                decimal exchangeOutput;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome to the Currency Exchanger! Please input one of the exchanges to begin:");
+                    Console.WriteLine("1-\n2-\n3-\n4-\n5-\n6-\n7-\n8-\n9-\n10-\n11-\n12"); //fill in with exchanges from Exchanger classes in order
+                    int.TryParse(Console.ReadLine(), out exchangeChoice);
+                } while (!(exchangeChoice >= 1 && exchangeChoice <= 12));
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Selected Exchange: "/*How would this be communicated?*/);
+                    Console.WriteLine("Insert input currency in " +
+                        (exchangeChoice <= 3 ? "USD" : exchangeChoice <= 6 ? "GBP" :
+                        exchangeChoice <= 9 ? "CAN" : "EUR"));
+                } while (decimal.TryParse(Console.ReadLine(), out exchangeInput));
+                exchangeOutput = 0/*monitor.[methodName](methodParameters)*/;
+                Console.WriteLine("New total is " + exchangeOutput.ToString("C"));
+                Console.WriteLine("Perform another exchange? (Y/N)"); //Consider how to account for bad input
+            } while (Console.ReadKey().Key == ConsoleKey.Y);
+            Console.WriteLine("Total exchanges performed: "/*Insert +  monitor.[propertyName]*/);
+            Console.WriteLine("Total currency exchanged: "/*Insert + monitor.[propertyName].ToString("C")*/ + "USD");
+            Console.ReadLine();
         }
     }
 }
